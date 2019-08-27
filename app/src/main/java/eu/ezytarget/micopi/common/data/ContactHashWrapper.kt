21f45b2a8d0a771717c2927cb4fc4873744569eb
Michel Sievers <michel@easy-target.eu>
2019-08-28 00:11:34 +0200

    Overriding hashCode.

--- app/src/main/java/eu/ezytarget/micopi/common/data/ContactHashWrapper.kt
+++ app/src/main/java/eu/ezytarget/micopi/common/data/ContactHashWrapper.kt
@@ -6,12 +6,42 @@ data class ContactHashWrapper(val contact: Contact): Serializable {
 
     private var hashModifier = 0
 
+    override fun hashCode(): Int {
+        return super.hashCode() + hashModifier
+    }
+
     fun initials(numberOfInitials: Int): String {
         if (numberOfInitials < 1) {
             return ""
         }
 
         return contact.name.first().toString()
+    }
+
+    /*
+    Technically not required, but we don't want to violate the general contract
+    for Object.hashCode(), do we?
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+
+        if (javaClass != other?.javaClass) {
+            return false
+        }
+
+        other as ContactHashWrapper
+
+        if (contact != other.contact) {
+            return false
+        }
+
+        if (hashModifier != other.hashModifier) {
+            return false
+        }
+
+        return true
     }
 
     fun increaseHashModifier() {
@@ -21,4 +51,5 @@ data class ContactHashWrapper(val contact: Contact): Serializable {
     fun decreaseHashModifier() {
         hashModifier--
     }
+
 }
