f2a9a6bd8cf084def2c1aa3c665cf7badf69e6c9
Michel Sievers <michel@easy-target.eu>
2019-08-31 14:32:14 +0200

    Change hash codes to be dependent on contact data

--- app/src/main/java/eu/ezytarget/micopi/common/data/Contact.kt
+++ app/src/main/java/eu/ezytarget/micopi/common/data/Contact.kt
@@ -7,4 +7,28 @@ data class Contact(
     val displayName: String
 ): Serializable {
 
+    override fun hashCode(): Int {
+        return entityID.hashCode() + displayName.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+        if (javaClass != other?.javaClass) {
+            return false
+        }
+
+        other as Contact
+
+        if (entityID != other.entityID) {
+            return false
+        }
+        if (displayName != other.displayName) {
+            return false
+        }
+
+        return true
+    }
+
 }
