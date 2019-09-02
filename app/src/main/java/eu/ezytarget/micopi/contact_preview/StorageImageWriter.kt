f31d2dc08bf15f195489a77660f83c8e3a8383b8
Michel Sievers <michel@easy-target.eu>
2019-09-03 00:43:29 +0200

    Add most of the file storage implementation without permission handling

--- app/src/main/java/eu/ezytarget/micopi/contact_preview/StorageImageWriter.kt
+++ app/src/main/java/eu/ezytarget/micopi/contact_preview/StorageImageWriter.kt
@@ -1,18 +1,57 @@
 package eu.ezytarget.micopi.contact_preview
 
 import android.graphics.Bitmap
+import android.os.Environment
+import java.io.File
+import java.io.FileOutputStream
+
 
 class StorageImageWriter {
 
+    var fileExtension = "PNG"
+    var compressionFormat = Bitmap.CompressFormat.PNG
+    var compressionQuality = 100
+    var publicDirectoryType = Environment.DIRECTORY_PICTURES!!
+    var publicSubDirectory = "Micopi"
+
+    fun saveBitmapToDevice(bitmap: Bitmap, fileNameWithoutExtension: String): Boolean {
+        val pictureRootDirectory = Environment.getExternalStoragePublicDirectory(
+            publicDirectoryType
+        )
+        val directory = File("${pictureRootDirectory}/${publicSubDirectory}}")
+        directory.mkdirs()
+
+        return saveBitmapToDevice(
+            bitmap,
+            directory,
+            fileNameWithoutExtension
+        )
+    }
+
     fun saveBitmapToDevice(
         bitmap: Bitmap,
-        fileName: String,
-        fileExtension: String = DEFAULT_FILE_EXTENSION
-    ) {
+        directory: File,
+        fileNameWithoutExtension: String
+    ): Boolean {
+        val fullFileName = "$fileNameWithoutExtension.$fileExtension"
+        val file = File(directory, fullFileName)
 
+        if (file.exists()) {
+            file.delete()
+        }
+
+        try {
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(compressionFormat, compressionQuality, outputStream)
+            outputStream.flush()
+            outputStream.close()
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
+
+        return true
     }
 
-    companion object {
-        const val DEFAULT_FILE_EXTENSION = "PNG"
-    }
 }
